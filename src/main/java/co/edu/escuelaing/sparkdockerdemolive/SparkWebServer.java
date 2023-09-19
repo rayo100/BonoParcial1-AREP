473ba161d80c10b42698542da7f26ca04bddb660
unknown <cesar.vasquez-m@labinfo.is.escuelaing.edu.co>
2023-09-19 16:04:24 -0500

    casi finalizado

--- src/main/java/co/edu/escuelaing/sparkdockerdemolive/SparkWebServer.java
+++ src/main/java/co/edu/escuelaing/sparkdockerdemolive/SparkWebServer.java
@@ -11,6 +11,32 @@ public class SparkWebServer {
     public static void main(String... args){
           port(getPort());
           get("hello", (req,res) -> "Hello Docker!");
+          get("/", (req,res) -> getIndexResponse());
+          //Sin
+          get("/sin/:numero", (req,res) -> {
+              double numero = Double.parseDouble(req.params("numero"));
+              return Math.sin(Math.toRadians(numero));
+          });
+          //Cos
+          get("/cos/:numero", (req,res) -> {
+              double numero = Double.parseDouble(req.params("numero"));
+              return Math.cos(Math.toRadians(numero));
+          });
+          //Palindrome
+          get("/magnitude/:str", (req,res) -> {
+              String str = req.params("str");
+              if(isPalindrome(str)){
+                  return "Es palindromo. ";
+              }
+              return "No es palindromo. ";
+          });
+          //Magnitud
+          get("/cos/:x/:y", (req,res) -> {
+              double xvar = Double.parseDouble(req.params("x"));
+              double yvar = Double.parseDouble(req.params("y"));
+              return Math.sqrt(xvar * xvar + yvar * yvar);
+          });
+          
           
     }
 
@@ -20,5 +46,27 @@ public class SparkWebServer {
         }
         return 4567;
     }
+
+    private static String getIndexResponse() {
+        return "";
+    }
+
+    public static boolean isPalindrome(String str) {
+  
+        str = str.replaceAll("\\s+", "").toLowerCase();
+        int left = 0, right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false; // No es un palíndromo
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+
+    
     
 }
